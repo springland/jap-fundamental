72d039b0f67f82790ce0f07994b6728636e79642
springland <fenglinmail@yahoo.com>
2022-05-15 21:52:36 -0400

    initial project setup

---
+++ src/main/java/com/springland365/jpafundamental/JpaFundamentalApplication.java
@@ -0,0 +1,13 @@
+package com.springland365.jpafundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFundamentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springland365/jpafundamental/JpaFundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springland365.jpafundamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
